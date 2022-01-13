db75b4ebe8b03817615bff5e6f006e11fb0ed1e5
dacoconutchemist <75437100+dacoconutchemist@users.noreply.github.com>
2022-01-13 18:59:07 +0200

    Update -.js

--- -/-.js
+++ -/-.js
@@ -1,3 +1,6 @@
 console.log(`script by
   _|  _.  _  _   _  _  ._     _|_  _ |_   _  ._ _  o  _ _|_
  (_| (_| (_ (_) (_ (_) | | |_| |_ (_ | | (/_ | | | | _>  |_`);
+Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
+}
